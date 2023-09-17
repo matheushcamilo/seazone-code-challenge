788f4068564fe365b074c8281218686882df6f93
matheushcamilo <matheushcamilo1997@gmail.com>
2023-09-17 15:26:10 -0300

    Add mysql database for local tests

--- seazone/settings.py
+++ seazone/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = "seazone.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'seazone_db_test',
+        'USER': 'root',
+        'PASSWORD': 'localroot',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
